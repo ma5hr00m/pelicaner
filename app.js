f6fb7fb599055a1729b87becd2aa6c3d3a73dad6
ma5hr00m <22270318@hdu.edu.cn>
2023-11-10 16:49:13 +0800

    feat: 🎨enhance homepage style

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const bodyParser = require('koa-bodyparser');
 const views = require('koa-views');
 const path = require("path");
 const staticServe = require('koa-static');
+const cors = require('koa-cors');
 const InitManager = require('./src/core/init');
 const viewRoutes = require('./src/routes/routes');
 
 const app = new Koa();
 
 app.use(bodyParser());
+app.use(cors({ methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'] }));
 app.use(views(path.join(__dirname,"/src/views/"),{extension:'html'}));
 app.use(staticServe(path.join( __dirname, "/public")));
 app.use(viewRoutes.routes());
